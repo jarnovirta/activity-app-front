d1843af677f37e5c77b94738cf18e87e29f06009
Jarno Virta <jarnovirta@users.noreply.github.com>
2019-03-03 18:16:07 +0200

    fixed typescript conf

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserTest from './components/user-profile/'
+import UserInterface from './components/user-profile/UserInterface'
 
 class App extends Component {
   render() {
+    const getUser = (): UserInterface => {
+      return {
+        name: 'Jarno',
+        age: 18,
+        address: "Pietarinkatu",
+        dob: new Date()
+
+      }
+    }
     return (
       <div className="App">
         <header className="App-header">
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            Hello <UserTest { ...getUser() } />
           </p>
           <a
             className="App-link"
--- src/components/user-profile/index.tsx
+++ src/components/user-profile/index.tsx
@@ -2,4 +2,20 @@
   Tiedot ja kuvan saa Get Authenticated Athlete avulla, DetailedAthlete objekti.
   Tähän sisään latest activities
 */
-export {}
+
+import * as React from 'react'
+import UserInterface from './UserInterface'
+
+class UserTest extends React.Component<UserInterface, {}> {
+  constructor(props: UserInterface) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        { this.props.name }
+      </div>
+    )
+  }
+}
+export default UserTest
