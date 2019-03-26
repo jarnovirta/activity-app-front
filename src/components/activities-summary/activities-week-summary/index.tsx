01c463d5fa58711aba9b2821297c2c4e01c4aced
Jarno Virta <jarnovirta@users.noreply.github.com>
2019-03-26 20:14:53 +0200

    add sports buttons to activity summary component

--- src/components/activities-summary/activities-week-summary/index.tsx
+++ src/components/activities-summary/activities-week-summary/index.tsx
@@ -22,10 +22,10 @@ const compareDates = (date1:Date, date2:Date):number => {
   d2.setHours(0, 0, 0, 0)
   return d1.getTime() - d2.getTime()
 }
+
 const getChartData = (activities:Array<StravaDetailedActivity>): Array<IChartData> => {
   let chartData:Array<IChartData> = []
   let date = new Date(Date.now() - 6 * 24 * 60 * 60 * 1000)
-
   const weekActivityData:Array<StravaDetailedActivity> =
     activities
     .filter(activity => {
@@ -33,41 +33,41 @@ const getChartData = (activities:Array<StravaDetailedActivity>): Array<IChartDat
     })
 
   for (let i = 0; i < 7; i++) {
-    let dist = 0
-    weekActivityData
+    let dist = weekActivityData
       .filter(activity => {
         return compareDates(new Date(activity.start_date_local), date) === 0
       })
-      .forEach(activity => dist += activity.distance)
-    chartData[i] = { day: days[date.getDay()], distance: dist}
+      .map(activity => activity.distance)
+      .reduce((sum, distance) => sum + distance, 0)
+
+    chartData[i] = {
+      day: days[date.getDay()],
+      distance: dist = Math.round(10 * dist / 1000) / 10
+    }
     date.setDate(date.getDate() + 1)
   }
   return chartData
 }
-const formatDistance = (activity:StravaDetailedActivity): number => {
-  return Math.round(10 * activity.distance / 1000) / 10
-}
-const getDay = (activity:StravaDetailedActivity): string => {
-  return days[new Date(activity.start_date_local).getDay()]
-}
+
 const ActivitiesWeekSummary: React.FunctionComponent<IProps> = (props: IProps) => {
   return (
     <div>
       <BarChart
         width={500}
-        height={300}
+        height={200}
         data={ getChartData(props.activities) }
         margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
+          top: 5, right: 30, left: 0, bottom: 5,
         }}
       >
         <XAxis dataKey="day" />
         <YAxis />
         <Tooltip />
-        <Bar dataKey={formatDistance} fill="#8884d8" />
+        <Bar dataKey="distance" fill="#8884d8" />
       </BarChart>
       <p className="text-muted">Strava</p>
     </div>
+
   )
 }
 const mapStateToProps = (state: any) => {
--- src/components/activities-summary/index.tsx
+++ src/components/activities-summary/index.tsx
@@ -1,24 +1,58 @@
+import './index.css'
 import React from 'react'
 import { connect } from 'react-redux'
-import { Card, ListGroup } from 'react-bootstrap'
+import { Card, ListGroup, ButtonGroup, Button } from 'react-bootstrap'
 import StravaDetailedActivity from '../../models/strava/strava-detailed-activity'
 import ActivitiesWeekSummary from './activities-week-summary'
+import FontAwesome from 'react-fontawesome'
+import { initializeActivities } from './../../reducers/activities-reducer'
 
 interface IProps {
   activities: Array<StravaDetailedActivity>
+  initialize: Function
+
 }
 const ActivitiesSummary: React.FunctionComponent<IProps> = (props: IProps) => {
+  const clickTest = (activity:string) => () => {
+    props.initialize()
+  }
   return (
+
     <div>
       <Card>
-        <Card.Header>Activities Summary</Card.Header>
+        <Card.Header>
+          Activities Summary
+        </Card.Header>
         <ListGroup variant="flush">
+          <ListGroup.Item>
+          <ButtonGroup>
+            <Button variant="secondary" onClick={clickTest('cycle')}>
+              <FontAwesome
+                name='bicycle'
+                size='2x'
+                style={{ color: 'white', textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+              />
+            </Button>
+            <Button variant="secondary" onClick={clickTest('run')}>
+              <FontAwesome
+                name='running'
+                size='2x'
+                style={{ color: 'white', textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+              />
+            </Button>
+            <Button variant="secondary" onClick={clickTest('swim')}>
+              <FontAwesome
+                name='swimmer'
+                size='2x'
+                style={{ color: 'white', textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+              />
+            </Button>
+          </ButtonGroup>
+          </ListGroup.Item>
           <ListGroup.Item><ActivitiesWeekSummary /></ListGroup.Item>
           <ListGroup.Item>Month summary</ListGroup.Item>
-          <ListGroup.Item>Year summary</ListGroup.Item>
         </ListGroup>
       </Card>
-
     </div>
   )
 }
@@ -27,4 +61,7 @@ const mapStateToProps = (state: any) => {
     activities: state.activities
   }
 }
-export default connect(mapStateToProps)(ActivitiesSummary)
+const mapDispatchersToProps = {
+  initialize: initializeActivities
+}
+export default connect(mapStateToProps, mapDispatchersToProps)(ActivitiesSummary)
