10a3825482c6ef5bfdf3a246f0c6bc8c9118403a
Jarno Virta <jarnovirta@users.noreply.github.com>
2019-03-27 19:36:24 +0200

    refactor activity-summary

--- src/components/activities-summary/activities-week-summary/index.tsx
+++ src/components/activities-summary/activities-week-summary/index.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import StravaDetailedActivity from '../../../models/strava/strava-detailed-activity'
 import {
-  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip,
+  BarChart, Bar, XAxis, YAxis, Tooltip,
 } from 'recharts'
 import { StravaActivityType } from '../../../models/strava/strava-activity-type';
 
 interface IProps {
-  activities: Array<IChartData>
+  activities: Array<StravaDetailedActivity>
 }
 interface IChartData {
   day: string,
@@ -15,7 +15,7 @@ interface IChartData {
 }
 
 const days = ["Sun", "Mon","Tue","Wed","Thu","Fri","Sat"]
-
+const startDate = new Date(Date.now() - 6 * 24 * 60 * 60 * 1000)
 const compareDates = (date1:Date, date2:Date):number => {
   let d1 = new Date(date1)
   d1.setHours(0, 0, 0, 0)
@@ -23,34 +23,34 @@ const compareDates = (date1:Date, date2:Date):number => {
   d2.setHours(0, 0, 0, 0)
   return d1.getTime() - d2.getTime()
 }
+const getChartData = (weekActivityData:Array<StravaDetailedActivity>):Array<IChartData> => {
+    const chartDate:Date = new Date(startDate)
+    let chartData:Array<IChartData> = []
+    for (let i = 0; i < 7; i++) {
+      let dist = weekActivityData
+        .filter(activity => {
+          return compareDates(new Date(activity.start_date_local), chartDate) === 0
+        })
+        .map(activity => activity.distance)
+        .reduce((sum, distance) => sum + distance, 0)
 
-const filterChartData = (
-  activities:Array<StravaDetailedActivity>,
-  activityType: StravaActivityType): Array<IChartData> => {
-  let chartData:Array<IChartData> = []
-  let date = new Date(Date.now() - 6 * 24 * 60 * 60 * 1000)
-  const weekActivityData:Array<StravaDetailedActivity> =
-    activities
-    .filter(activity => {
-      return activity.type === activityType && compareDates(new Date(activity.start_date_local), date) >= 0
-
-    })
-
-  for (let i = 0; i < 7; i++) {
-    let dist = weekActivityData
-      .filter(activity => {
-        return compareDates(new Date(activity.start_date_local), date) === 0
-      })
-      .map(activity => activity.distance)
-      .reduce((sum, distance) => sum + distance, 0)
-
-    chartData[i] = {
-      day: days[date.getDay()],
-      distance: dist = Math.round(10 * dist / 1000) / 10
+      chartData[i] = {
+        day: days[chartDate.getDay()],
+        distance: dist = Math.round(10 * dist / 1000) / 10
+      }
+      chartDate.setDate(chartDate.getDate() + 1)
     }
-    date.setDate(date.getDate() + 1)
+    return chartData
   }
-  return chartData
+
+const filterActivities = (
+  activities:Array<StravaDetailedActivity>,
+  activityType: StravaActivityType): Array<StravaDetailedActivity> => {
+    return activities
+      .filter(activity => {
+        return activity.type === activityType &&
+          compareDates(new Date(activity.start_date_local), startDate) >= 0
+      })
 }
 
 const ActivitiesWeekSummary: React.FunctionComponent<IProps> = (props: IProps) => {
@@ -59,7 +59,7 @@ const ActivitiesWeekSummary: React.FunctionComponent<IProps> = (props: IProps) =
       <BarChart
         width={500}
         height={200}
-        data={ props.activities }
+        data={ getChartData(props.activities) }
         margin={{
           top: 5, right: 30, left: 0, bottom: 5,
         }}
@@ -71,12 +71,13 @@ const ActivitiesWeekSummary: React.FunctionComponent<IProps> = (props: IProps) =
       </BarChart>
       <p className="text-muted">Strava</p>
     </div>
-
   )
 }
 const mapStateToProps = (state: any) => {
   return {
-    activities: filterChartData(state.activities.activities, state.activitiesSummary.selectedActivity)
+    activities: filterActivities(
+      state.activities.activities,
+      state.activitiesSummary.selectedActivity)
   }
 }
 export default connect(mapStateToProps)(ActivitiesWeekSummary)
--- src/services/activities/index.tsx
+++ src/services/activities/index.tsx
@@ -2,16 +2,23 @@ import { StravaActivityType } from "../../models/strava/strava-activity-type";
 import StravaDetailedActivity from "../../models/strava/strava-detailed-activity";
 
 const getAll = async ():Promise<Array<StravaDetailedActivity>> => {
-  let date:Date = new Date()
-  let activities:Array<StravaDetailedActivity> = []
+  let activities:Array<StravaDetailedActivity> = getActivityList(StravaActivityType.Ride)
+  activities = activities.concat(getActivityList(StravaActivityType.Run))
+  activities = activities.concat(getActivityList(StravaActivityType.Swim))
 
+  return activities
+}
+
+const getActivityList = (activityType:StravaActivityType):Array<StravaDetailedActivity> => {
+  let activities:Array<StravaDetailedActivity> = []
+  let date:Date = new Date()
   for (let i = 0; i < 30; i++) {
     activities.push({
       "name" : "Happy Friday",
       "distance" : Math.floor(Math.random() * 30000 * 10) / 10,
       "moving_time" : Math.floor(Math.random() * 2 * 60 * 60 * 10) / 10,
       "elapsed_time" : Math.floor(Math.random() * 2 * 60 * 60 * 10) / 10,
-      "type" : StravaActivityType.Ride,
+      "type" : activityType,
       "workout_type" : 2,
       "id" : 154504250376823,
       "start_date_local" : date.toString(),
